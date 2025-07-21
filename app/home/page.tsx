f333975a46f1b74d10aa2ce9c0f335fdf9808542
Jaid199 <108267929+Jaid199@users.noreply.github.com>
2025-07-21 11:23:47 -1200

    fixing the issue

--- app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegistrationPortal from "../registration-portal";
-
-export default function HomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-deepBlue p-4">
-      <RegistrationPortal />
-    </div>
-  );
-}
