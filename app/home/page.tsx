36b04ec5fdcfab1e71b0d9d5c0a3b4f1401d4fcb
Jaid199 <108267929+Jaid199@users.noreply.github.com>
2025-07-21 11:19:26 -1200

    2nd commit components page added and no colours are done

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import RegistrationPortal from "../registration-portal";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-deepBlue p-4">
+      <RegistrationPortal />
+    </div>
+  );
+}
