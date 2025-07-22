6932aec28bde99b4d3525fa016c2b91a1a25efbd
Rashnan <rashnan.m.s@gmail.com>
2025-07-23 02:38:58 +0800

    Made a simple home page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'races.3000tfg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'external-content.duckduckgo.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
